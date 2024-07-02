ec146e293b7cd03968b1a1c79ccf33e5f289370e
desipeli <mr.desipeli@gmail.com>
2024-07-02 13:39:10 +0300

    pvm- ja aikadialogit siirretty omaan tiedostoon

---
+++ app/src/main/java/com/example/sokerihiiri/ui/components/DateAndTimePickers.kt
@@ -0,0 +1,92 @@
+package com.example.sokerihiiri.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerLayoutType
+import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import java.time.LocalTime
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppDatePickerDialog(
+    showState: MutableState<Boolean>,
+    initialState: Long = System.currentTimeMillis(),
+    onDateSelected: (Long) -> Unit,
+    onDismissRequest: () -> Unit = { showState.value = false },
+    confirmButtonText: String = "OK",
+    dismissButtonText: String = "Peruuta"
+) {
+    val datePickerState = rememberDatePickerState(initialSelectedDateMillis = initialState)
+
+
+    DatePickerDialog(
+        onDismissRequest = { showState.value = false },
+        confirmButton = {
+            TextButton(onClick = {
+                showState.value = false
+                val selectedMillis = datePickerState.selectedDateMillis
+                if (selectedMillis != null) {
+                    onDateSelected(selectedMillis)
+                }
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { showState.value = false }) {
+                Text("Peruuta")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTimePickerDialog(
+    showState: MutableState<Boolean>,
+    initialHour: Int = LocalTime.now().hour,
+    initialMinute: Int = LocalTime.now().minute,
+    onTimeSelected: (Int, Int) -> Unit
+) {
+    val timePickerState = rememberTimePickerState(
+        is24Hour = true,
+        initialHour = initialHour,
+        initialMinute = initialMinute)
+
+    AlertDialog(
+        text = {
+            Column {
+                TimePicker(
+                    state = timePickerState,
+                    layoutType = TimePickerLayoutType.Vertical,
+                )
+            }
+        },
+        onDismissRequest = { showState.value = false },
+        confirmButton = {
+            Button(onClick = {
+                showState.value = false
+                onTimeSelected(
+                    timePickerState.hour,
+                    timePickerState.minute
+                )
+            }) {
+                Text("OK")
+            }
+        }
+    )
+}
--- app/src/main/java/com/example/sokerihiiri/ui/screens/Measurement/MeasurementScreen.kt
+++ app/src/main/java/com/example/sokerihiiri/ui/screens/Measurement/MeasurementScreen.kt
@@ -11,32 +11,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import java.text.SimpleDateFormat
 import java.util.Locale
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.DatePickerDialog
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TimePickerLayoutType
-import androidx.compose.material3.rememberDatePickerState
-import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.unit.dp
+import com.example.sokerihiiri.ui.components.AppDatePickerDialog
+import com.example.sokerihiiri.ui.components.AppTimePickerDialog
 import com.example.sokerihiiri.ui.components.NumberTextField
 import java.util.Date
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MeasurementScreen(
     measurementViewModel: MeasurementViewModel,
@@ -50,13 +41,8 @@ fun MeasurementScreen(
 
     val dateFormatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
 
-    val datePickerState = rememberDatePickerState(initialSelectedDateMillis = uiState.date)
-    var showDatePicker by remember { mutableStateOf(false) }
-    val timePickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = uiState.hour,
-        initialMinute = uiState.minute)
-    var showTimePicker by remember { mutableStateOf(false) }
+    var showDatePicker: MutableState<Boolean> = remember { mutableStateOf(false) }
+    var showTimePicker: MutableState<Boolean> = remember { mutableStateOf(false) }
 
 
     fun handleValueChange(newValue: String) {
@@ -104,7 +90,7 @@ fun MeasurementScreen(
 
             Spacer(modifier = Modifier.height(64.dp))
 
-            TextButton(onClick = { showTimePicker = true }) {
+            TextButton(onClick = { showTimePicker.value = true }) {
                 Text(
                     text = String.format(
                         Locale.getDefault(),
@@ -114,7 +100,7 @@ fun MeasurementScreen(
                     )
                 )
             }
-            TextButton(onClick = { showDatePicker = true }) {
+            TextButton(onClick = { showDatePicker.value = true }) {
                 Text(dateFormatter.format(Date(uiState.date)))
             }
             
@@ -151,48 +137,20 @@ fun MeasurementScreen(
         }
     }
 
-    if (showDatePicker) {
-        DatePickerDialog(
-            onDismissRequest = { showDatePicker = false },
-            confirmButton = {
-                TextButton(onClick = {
-                    showDatePicker = false
-                    val selectedMillis = datePickerState.selectedDateMillis
-                    if (selectedMillis != null) {
-                        measurementViewModel.setDate(selectedMillis)
-                    }
-                }) {
-                    Text("OK")
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = { showDatePicker = false }) {
-                    Text("Peruuta")
-                }
-            }
-        ) {
-            DatePicker(state = datePickerState)
-        }
+    if (showDatePicker.value) {
+        AppDatePickerDialog(
+            showState = showDatePicker,
+            initialState = uiState.date,
+            onDateSelected = {measurementViewModel.setDate(it)} )
     }
 
-    if (showTimePicker) {
-        AlertDialog(
-            text = {
-                Column {
-                    TimePicker(
-                        state = timePickerState,
-                        layoutType = TimePickerLayoutType.Vertical,
-                    )
-                }
-            },
-            onDismissRequest = { showTimePicker = false },
-            confirmButton = {
-                Button(onClick = {
-                    measurementViewModel.setTime(timePickerState.hour, timePickerState.minute)
-                    showTimePicker = false
-                }) {
-                    Text("OK")
-                }
+    if (showTimePicker.value) {
+        AppTimePickerDialog(
+            showState = showTimePicker,
+            initialHour = uiState.hour,
+            initialMinute = uiState.minute,
+            onTimeSelected = { hour, minute ->
+                measurementViewModel.setTime(hour, minute)
             }
         )
     }
